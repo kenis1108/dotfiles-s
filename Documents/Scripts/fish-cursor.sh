9a72b07ae948dcf6ac7e6b2b26c1935424e1e083
kenis0225 <ken.huang1@ecoflow.com>
2026-03-12 08:26:42 +0000

    feat(raycast): update script

--- Documents/Scripts/fish-cursor.sh
+++ Documents/Scripts/fish-cursor.sh
@@ -2,12 +2,12 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title VSCode (fish)
+# @raycast.title Cursor (fish)
 # @raycast.mode silent
 
 # Optional parameters:
 # @raycast.icon 🐟
-# @raycast.description Launch VSCode using fish shell environment
+# @raycast.description Launch Cursor using fish shell environment
 
 # 确保 fish 是 login shell，加载完整环境
-exec fish -l -c "code $argv"
+exec fish -l -c "cursor $argv"
