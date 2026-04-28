efda4965233e6eadd90d46249bd4620ef7d13832
kenis <1836362346@qq.com>
2026-04-28 11:38:28 +0800

    Merge from main
    feat: add neovim & starship

---
+++ dot_config/bash/config.bash
@@ -0,0 +1,9 @@
+# -------- variable --------
+export EDITOR=nvim
+export PATH="$HOME/.local/bin:$PATH"
+
+# -------- application --------
+source "$HOME/.config/bash/application"
+
+# -------- alias --------
+source "$HOME/.config/bash/alias"
