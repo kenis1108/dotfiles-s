a8a1c54106991a87296020fad0f16fc857ffc868
kenis0225 <ken.huang1@ecoflow.com>
2026-05-06 18:41:38 +0800

    Update setup-origin_configs.sh

--- dot_config/nvim_nvchad/setup-origin_configs.sh
+++ dot_config/nvim_nvchad/setup-origin_configs.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-ln -sf $HOME/.config/nvim/lua/configs $HOME/.config/nvim_nvchad/lua/origin_configs
+ln -sf $HOME/.config/nvim/lua $HOME/.config/nvim_nvchad/lua/origin_configs
+
+ln -sf $HOME/.config/nvim/lua $HOME/.config/nvim_lazyvim/lua/origin_configs
