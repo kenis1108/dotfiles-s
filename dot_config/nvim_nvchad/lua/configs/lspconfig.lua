1350dfb07dc1473b84343d0b66fcb54d4f2afa83
kenis0225 <ken.huang1@ecoflow.com>
2026-05-06 18:43:13 +0800

    Delete dot_config/nvim_nvchad/lua/configs/lspconfig.lua

--- dot_config/nvim_nvchad/lua/configs/lspconfig.lua
+++
@@ -1,6 +0,0 @@
-require("nvchad.configs.lspconfig").defaults()
-
-local servers = { "lua_ls", "vue_ls", "jsonls" }
-vim.lsp.enable(servers)
-
--- read :h vim.lsp.config for changing options of lsp servers
