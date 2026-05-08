72f09894779e233d4ec6cca42ea65276bcf3a07c
kenis0225 <ken.huang1@ecoflow.com>
2026-05-08 10:22:05 +0800

    Set local leader key conditionally in options.lua

--- dot_config/nvim/lua/configs/options.lua
+++ dot_config/nvim/lua/configs/options.lua
@@ -2,7 +2,9 @@
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+if not vim.g.lazy_did_setup then
+  vim.g.maplocalleader = "\\"
+end
 
 vim.opt.number = true
 -- Make line numbers default
