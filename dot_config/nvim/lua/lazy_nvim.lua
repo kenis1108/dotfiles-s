b79e9b7f13e7f373825bd7c33faea3144b3afa1c
kenis0225 <ken.huang1@ecoflow.com>
2026-05-08 18:38:12 +0800

    Refactor lazy_nvim.lua to enable/disable plugins

--- dot_config/nvim/lua/lazy_nvim.lua
+++ dot_config/nvim/lua/lazy_nvim.lua
@@ -1,94 +1,13 @@
 return {
   { "nvim-tree/nvim-tree.lua", enabled = false },
+
   { "nvim-treesitter/nvim-treesitter", enabled = false },
-  {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      "mason.nvim",
-      {
-        "mason-org/mason-lspconfig.nvim",
-        lazy = false,
-        opts = {},
-      },
-    },
-  },
-  {
-    "mason-org/mason.nvim",
-    lazy = false,
-    opts_extend = { "ensure_installed" },
-    opts = {
-      ensure_installed = {
-        "lua-language-server",
-        "stylua",
-      },
-    },
-    ---@param opts MasonSettings | {ensure_installed: string[]}
-    config = function(_, opts)
-      require("mason").setup(opts)
-      local mr = require "mason-registry"
-      mr:on("package:install:success", function()
-        vim.defer_fn(function()
-          -- trigger FileType event to possibly load this newly installed LSP server
-          require("lazy.core.handler.event").trigger {
-            event = "FileType",
-            buf = vim.api.nvim_get_current_buf(),
-          }
-        end, 100)
-      end)
-
-      -- 自动安装非 LSP 工具（Formatter、Linter、DAP 等）
-      mr.refresh(function()
-        for _, tool in ipairs(opts.ensure_installed) do
-          local p = mr.get_package(tool)
-          if not p:is_installed() then
-            p:install()
-          end
-        end
-      end)
-    end,
-  },
-  {
-    "folke/snacks.nvim",
-    enabled = vim.env.NVIM_APPNAME and vim.env.NVIM_APPNAME:find "lazyvim" ~= nil,
-    opts = {
-      dashboard = { enabled = false },
-      explorer = { enabled = false },
-    },
-  },
-
-  {
-    "mikavilpas/yazi.nvim",
-    version = "*",
-    event = "VeryLazy",
-    dependencies = {
-      { "nvim-lua/plenary.nvim", lazy = true },
-    },
-    keys = {
-      { mode = { "n", "v" }, "<leader>E", "<cmd>Yazi cwd<cr>", desc = "Open Yazi in working directory" },
-      { mode = { "n", "v" }, "<leader>e", "<cmd>Yazi<cr>", desc = "Open Yazi in current file directory" },
-    },
-    opts = {
-      -- if you want to open yazi instead of netrw, see below for more info
-      open_for_directories = false,
-      keymaps = {
-        show_help = "<f2>",
-      },
-    },
-    -- 👇 if you use `open_for_directories=true`, this is recommended
-    init = function()
-      -- mark netrw as loaded so it's not loaded at all.
-      --
-      -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
-      vim.g.loaded_netrwPlugin = 1
-    end,
-  },
 
   {
     "nvim-telescope/telescope.nvim",
-    enabled = vim.env.NVIM_APPNAME and vim.env.NVIM_APPNAME:find "nvchad" ~= nil,
+    enabled = false,
     keys = {
-      { "<leader>fb", "<cmd>Telescope builtin<cr>", desc = "Telescope find builtin" },
+      { "<leader>f", "<cmd>Telescope builtin<cr>", desc = "Telescope find builtin" },
       -- 文件查找
       {
         "<leader>fc",
@@ -137,6 +56,99 @@ return {
       require("telescope").setup(opts)
     end,
   },
+  
+  {
+    "folke/snacks.nvim",
+    lazy = false,
+    opts = {
+      dashboard = { enabled = false },
+      explorer = { enabled = false },
+      picker = { enable = true },
+      scroll = { enable = true }
+    },
+    keys = {
+      { "<leader>f", function() Snacks.picker() end, desc = "Open Snacks Picker" },
+      { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
+      { "<leader>fd", function() Snacks.picker.files({ cwd = vim.fn.stdpath("data") }) end, desc = "Find Config Data" },
+      { "<leader>/c", function() Snacks.picker.grep({ cwd = vim.fn.stdpath("config") }) end, desc = "Grep Config File" },
+      { "<leader>/d", function() Snacks.picker.grep({ cwd = vim.fn.stdpath("data") }) end, desc = "Grep Config Data" },
+    }
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "mason.nvim",
+      {
+        "mason-org/mason-lspconfig.nvim",
+        lazy = false,
+        opts = {},
+      },
+    },
+  },
+  {
+    "mason-org/mason.nvim",
+    lazy = false,
+    opts_extend = { "ensure_installed" },
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+      },
+    },
+    ---@param opts MasonSettings | {ensure_installed: string[]}
+    config = function(_, opts)
+      require("mason").setup(opts)
+      local mr = require "mason-registry"
+      mr:on("package:install:success", function()
+        vim.defer_fn(function()
+          -- trigger FileType event to possibly load this newly installed LSP server
+          require("lazy.core.handler.event").trigger {
+            event = "FileType",
+            buf = vim.api.nvim_get_current_buf(),
+          }
+        end, 100)
+      end)
+
+      -- 自动安装非 LSP 工具（Formatter、Linter、DAP 等）
+      mr.refresh(function()
+        for _, tool in ipairs(opts.ensure_installed) do
+          local p = mr.get_package(tool)
+          if not p:is_installed() then
+            p:install()
+          end
+        end
+      end)
+    end,
+  },
+  {
+    "mikavilpas/yazi.nvim",
+    version = "*",
+    event = "VeryLazy",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", lazy = true },
+    },
+    keys = {
+      { mode = { "n", "v" }, "<leader>E", "<cmd>Yazi cwd<cr>", desc = "Open Yazi in working directory" },
+      { mode = { "n", "v" }, "<leader>e", "<cmd>Yazi<cr>", desc = "Open Yazi in current file directory" },
+    },
+    opts = {
+      -- if you want to open yazi instead of netrw, see below for more info
+      open_for_directories = false,
+      keymaps = {
+        show_help = "<f2>",
+      },
+    },
+    -- 👇 if you use `open_for_directories=true`, this is recommended
+    init = function()
+      -- mark netrw as loaded so it's not loaded at all.
+      --
+      -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+      vim.g.loaded_netrwPlugin = 1
+    end,
+  },
+
 
   {
     "folke/which-key.nvim",
