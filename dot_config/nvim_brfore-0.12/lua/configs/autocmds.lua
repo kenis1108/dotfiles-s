14e7175d25e0cf80e6f5069aabc76fb86ad0f86b
kenis <1836362346@qq.com>
2026-05-11 15:25:34 +0800

    remove useless

--- dot_config/nvim_brfore-0.12/lua/configs/autocmds.lua
+++
@@ -1,7 +0,0 @@
--- Highlight on yank
-vim.api.nvim_create_autocmd("TextYankPost", {
-  group = vim.api.nvim_create_augroup('highlight_yank', { clear = true }),
-  callback = function()
-    (vim.hl or vim.highlight).on_yank()
-  end,
-})
